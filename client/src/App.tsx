ee5f5bdd21d84bfa59c3792dfc62485997d03056
Adelodun Olusola-Ajayi <43054789-delodunoa@users.noreply.replit.com>
2025-06-13 00:48:35 +0000

    Restored to '11a81ca3fcf14f2e64d275bc376d587fa2921a8b'
    Replit-Restored-To: 11a81ca3fcf14f2e64d275bc376d587fa2921a8b

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,29 @@
 import { Switch, Route } from "wouter";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import Navigation from "./components/navigation";
-import Home from "./pages/home";
-import Professionals from "./pages/professionals";
-import CreateProfessionalProfile from "./components/create-professional-profile";
-import NotFound from "./pages/not-found";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Home from "@/pages/home";
+import Professionals from "@/pages/professionals";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/professionals" component={Professionals} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen bg-background">
-        <Navigation />
-        <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/professionals" component={Professionals} />
-          <Route path="/create-profile" component={CreateProfessionalProfile} />
-          <Route component={NotFound} />
-        </Switch>
+      <TooltipProvider>
         <Toaster />
-      </div>
+        <Router />
+      </TooltipProvider>
     </QueryClientProvider>
   );
 }
